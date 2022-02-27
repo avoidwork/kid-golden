095542185e64c00f71f9bf8f7c1b839518c6f5a1
Jason Mulligan <jason.mulligan@avoidwork.com>
2022-02-26 21:00:23 -0500

    Laying out the sample page

--- samples/samples.js
+++ samples/samples.js
@@ -1,17 +1,40 @@
 "use strict";
 
 (async function () {
+	const render = window.requestAnimationFrame;
+
 	function log (arg = "", type = "log") {
 		console[type](typeof arg !== "object" ? `${new Date().getTime()}: ${arg.toString()}` : arg);
+	}
+
+	function display (type = "", el = {}, {title = "", description = "", data = []}) {
+		const tpl = templates.get("diagram").cloneNode(true);
+
+		tpl.querySelector("h2").innerText = title;
+		tpl.querySelector("p").innerText = description;
+		tpl.querySelector("div[data-id='render']").innerHTML = `<pre><code>${JSON.stringify(data)}</code></pre>`;
+
+		render(() => {
+			el.appendChild(tpl);
+			log(`Rendered ${type} diagram`);
+		});
 	}
 
 	log("Hello! Lets render some diagrams!");
 
 	const templates = new Map(Array.from(document.querySelectorAll("template")).map(i => [i.dataset.id, i.content.cloneNode(true)])),
 		res = await fetch("samples.json"),
-		data = await res.json();
+		data = await res.json(),
+		ctx = document.querySelector("section");
 
 	log("Templates & data loaded");
 	log(templates);
 	log(data);
+	log("Rendering types of diagrams");
+
+	for (const key of Object.keys(data)) {
+		for (const val of data[key].values()) {
+			display(key, ctx, val);
+		}
+	}
 }());
